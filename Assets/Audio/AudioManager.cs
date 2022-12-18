3ebd71b03f531d5a01f435e8ede75d18c8274934
The-Architect01 <bircng01@pfw.edu>
2022-12-18 15:38:10 -0500

    Updated Autio settings

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -2,30 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
-{
+public class AudioManager : MonoBehaviour {
 
     public AudioSource BGM;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Awake() {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if (musicObj.Length > 1) Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void ChangeBGM(AudioClip music)
-    {
+    public void ChangeBGM(AudioClip music) {
         if (BGM.clip.name != music.name) { 
             BGM.Stop();
             BGM.clip = music;
             BGM.Play();
-           }
+        }
     }
 
 
